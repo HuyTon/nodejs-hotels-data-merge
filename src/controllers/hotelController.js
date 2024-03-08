930aa16d235b9bb03cd326df7b3a440987bd5557
Huy Ton That <huytonit@gmail.com>
2024-03-08 22:36:55 +0800

    Implemented filter hotels by amenities

--- src/controllers/hotelController.js
+++ src/controllers/hotelController.js
@@ -2,7 +2,7 @@ const HotelService = require("../services/hotelService");
 
 module.exports.getHotels = async (req, res, next) => {
   try {
-    let { destination, hotels, page = 1, limit = 10 } = req.query;
+    let { destination, hotels, amenities, page = 1, limit = 10 } = req.query;
 
     // Validate page and limit parameters
     page = parseInt(page);
@@ -11,27 +11,34 @@ module.exports.getHotels = async (req, res, next) => {
     if (isNaN(page) || page < 1) {
       return res.status(400).json({ error: "Invalid page number" });
     }
-
     if (isNaN(limit) || limit < 0) {
       return res.status(400).json({ error: "Invalid limit value" });
     }
 
     const hotelsData = await HotelService.getAllHotels();
-
     if (!hotelsData) {
       fetchDataFailed(res);
+      return;
     }
 
+    // Filter hotels by IDs
     let filteredHotels = hotels
       ? filterHotelsById(hotelsData, hotels)
       : hotelsData;
 
-    filteredHotels = destination
-      ? filterHotelsByDestination(filteredHotels, destination)
-      : filteredHotels;
+    // Filter hotels by destination ID
+    if (destination) {
+      filteredHotels = filterHotelsByDestination(filteredHotels, destination);
+    }
 
+    // Filter hotels by amenities
+    if (amenities) {
+      filteredHotels = filterHotelsByAmenities(filteredHotels, amenities);
+    }
+
+    // Calculate start and end of indexes for pagination
     const startIndex = (page - 1) * limit;
-    const endIndex = limit > 0 ? page * limit : hotelsData.length;
+    const endIndex = limit > 0 ? page * limit : filteredHotels.length;
 
     const hotelsSubset = filteredHotels.slice(startIndex, endIndex);
 
@@ -50,6 +57,58 @@ const filterHotelsByDestination = (hotels, destinationId) => {
   return hotels.filter(
     (hotel) => parseInt(hotel.destination_id) == parseInt(destinationId)
   );
+};
+
+const filterHotelsByAmenities = (hotels, amenities) => {
+  const filteredHotels = [];
+  const matchedHotels = [];
+
+  const amenityArray = amenities
+    .split(",")
+    .map((amenity) => amenity.toLowerCase());
+
+  if (amenityArray.length === 0) {
+    return hotels;
+  }
+
+  hotels.forEach((hotel) => {
+    const matchedGeneralCount = hotel.amenities.general
+      ? countMatchedAmenities(amenityArray, hotel.amenities.general)
+      : 0;
+
+    const matchedRoomCount = hotel.amenities.room
+      ? countMatchedAmenities(amenityArray, hotel.amenities.room)
+      : 0;
+
+    const matchedTotalCount = matchedGeneralCount + matchedRoomCount;
+
+    if (matchedTotalCount > 0) {
+      matchedHotels.push({
+        hotel: hotel,
+        matchedCount: matchedTotalCount,
+      });
+    }
+  });
+
+  matchedHotels.sort((a, b) => {
+    return b.matchedCount >= a.matchedCount ? 1 : -1;
+  });
+
+  matchedHotels.forEach((hotel) => {
+    filteredHotels.push(hotel.hotel);
+  });
+
+  return filteredHotels;
+};
+
+const countMatchedAmenities = (arr1, arr2) => {
+  let count = 0;
+  for (const element of arr1) {
+    if (arr2.includes(element.toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
 };
 
 const fetchDataFailed = (res, error = null) => {
